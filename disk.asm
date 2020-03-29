f77c28155b2e0dd7591f7fb08fb19350975687c1
Cristhian Grundmann <cristhiangrundmann@gmail.com>
2020-03-29 17:46:52 -0300

    New version. No partition table abuse. Only cursor limitation left do to.

--- disk.asm
+++ disk.asm
@@ -1,191 +1,166 @@
 org 0x7c00
 bits 16
 
-%define SECTORS 2
-%define COLOR_ASCII 0x20
-%define COLOR_HEX_A 0x10
-%define COLOR_HEX_B 0x30
-%define COLOR_CURSOR 0x40
-%define COLOR_VOID 0x50
-%define COLOR_DE 0x00
-%define COLOR_ED 0x0f
+%define SECTORS 0x1
+
+%define BCOLORAS 0x20
+%define BCOLORH1 0x10
+%define BCOLORH2 0x30
+%define BCOLORCU 0x40
+%define FCOLORDE 0x00
+%define FCOLORED 0x05
+
 
 main:
-    ;SEGMENTS
     xor ax, ax
     mov bp, ax
     mov sp, bp
     mov ah, 0xb8
     mov es, ax
-    mov ah, 0x7
-    mov ss, ax
+    mov ah, 0x07
     mov ds, ax
-
+    mov ss, ax
     cld
+    mov di, 0x28
 
-draw:
-
-    ;CLEAR SCREEN
+clear:
     mov ax, 0x3
     int 0x10
+    mov ax, 0x1112
+    mov bl, 0x0
+    int 0x10
+draw:
     pusha
     push es
-    push 0xb800 + 0xa*0x7
-    pop es
-    mov ah, COLOR_DE
-    mov si, TARGET - 0x7000 + 0x7
-    mov di, 0x8e
-    mov cl, 0x7
+    mov bx, 0xb800 + 0xa*0x2
+    mov es, bx
+    mov ah, FCOLORDE
+    mov si, TARGET - 0x7000
+    mov di, 0x28
+    mov cl, 0x8
 draw0:
     lodsb
-    dec si
     call type
-    dec di
-    dec di
-    dec si
     dec cl
-    jnl draw0
-    mov si, 0x1ff
+    jnz draw0
+    mov bl, 0xa*0x4
+    mov es, bx
+    xor si, si
+    mov ch, 0x20
 draw1:
-    mov di, 0x7e
+    mov cl, 0x10
+    mov di, 0x28
 draw2:
     lodsb
-    dec si
     call type
-    dec si
-    dec di
-    dec di
-    jnl draw2
-    mov bx, es
-    sub bl, 0xa
+    dec cl
+    jnz draw2
+    add bx, 0xa
     mov es, bx
-    jnl draw1
-draw3:
+    dec ch
+    jnz draw1
     pop es
     popa
 
-loop:
-    mov ax, es
-    cmp di, 0xa0
-    jne loopcm1
-    xor di, di
-loopcm1:
-    cmp di, -0x2
-    jne loopcm2
-    mov di, 0xa0-0x2
-loopcm2:
-    cmp al, 0xa * (-0x1)
-    jne loopc0
-    mov al, 0xa * 0x17
-loopc0:
-    cmp al, 0xa * 0x18
-    jne loopc1
-    mov al, 0x0
-loopc1:
-    mov es, ax
+mloop:
 
-    ;CURSOR
-    mov dh, [es:di+0x1]
-    mov cl, dh
-    and cl, 0x0f
-    or cl, COLOR_CURSOR
-    mov [es:di+0x1], cl
+    
+
+    mov ax, [es:di]
+    push ax
+    and ah, 0x0f
+    or ah, BCOLORCU
+    mov [es:di], ax
 
     xor ax, ax
     int 0x16
 
-    mov [es:di+0x1], dh
+    pop word [es:di]
 
     cmp al, 0x20
-    jb loop1
-    
-    mov cx, es
-    cmp cl, 0xa * 0x8
-    jb loop0
+    jb mloop1
 
-    mov ch, al
+    cmp di, 0x48
+    jb mloop0
+    mov ah, al
     call hexascii
     and al, 0x0f
     call hexascii
-    cmp al, ch
-    jne loop
+    cmp ah, al
+    jne mloop
 
-    stosb
-    dec di
-    mov cl, 0x1
     push di
-    and di, 0xffff - 0x2
-    mov si, ADDRESS - 0x7000 + 0x1
+    stosb
+    mov cl, 0x1
+    mov si, mloop_addr - 0x7000 + 0x1
+    and di, 0xffff - 0x3
     call read
     call swap
-ADDRESS:
-    mov ax, COLOR_ED * 0x100
+mloop_addr:
+    mov ax, FCOLORED * 0x100 + '?'
     call type
-    call swap
     pop di
+mloop_incdi:
     inc di
     inc di
-    jmp loop
-loop0:
-    mov ah, COLOR_ED
+    jmp mloop
+mloop0:
+    mov ah, FCOLORED
     call type
-    inc di
-    inc di
-    jmp loop
-loop1:
+    jmp mloop
+
+mloop1:
     cmp ah, 0x4b
-    jne loop2
+    jne mloop2
     dec di
     dec di
-loop2:
+    jmp mloop
+mloop2:
     cmp ah, 0x4d
-    jne loop3
-    inc di
-    inc di
-loop3:
+    je mloop_incdi
     cmp ah, 0x48
-    jne loop4
+    jne mloop3
     mov cx, es
-    sub cl, 0xa
+    sub cx, 0xa
     mov es, cx
-loop4:
+mloop3:
     cmp ah, 0x50
-    jne loop5
+    jne mloop4
     mov cx, es
-    add cl, 0xa
+    add cx, 0xa
     mov es, cx
-loop5:
+mloop4:
     cmp ah, 0x0f
-    jne loop6
+    jne mloop5
     call swap
-loop6:
+mloop5:
     cmp ah, 0x42
-    je loop7
+    je mloop7
     cmp ah, 0x43
-    jne loop8
+    jne mloop8
     pusha
     push es
     push 0xb800
     pop es
-    mov di, 0xa0*0x8
-    mov cl, 0x20
+    mov di, 0xa0*0x4 + 0x50
+    mov cl, 0x10
     xor si, si
-loop6a:
+mloop6:
     call read
     add di, 0xa0
-    add si, 0x20
+    add si, 0x10
     cmp si, 0x200
-    jne loop6a
+    jne mloop6
     pop es
     popa
-loop7:
+mloop7:
     pusha
     push es
-    push 0xb800 + 0xa*0x17 + 0x8
+    push 0xb800 + 0xa*0x2
     pop es
     mov cl, 0x8
     mov si, TARGET - 0x7000
-    xor di, di
+    mov di, 0x50
     call read
     pop es
     mov al, 0x0
@@ -194,26 +169,25 @@ loop7:
     jc main
     popa
     jmp draw
-loop8:
+mloop8:
     cmp ah, 0x1
-    jne loop
+    jne mloop
     call 0x7000
-    jmp loop
+    jmp mloop
 
 read:
     pusha
 read0:
-    mov al, [es:di]
+    mov ax, [es:di]
+    stosw
     call hexascii
     shl al, 0x4
-    mov ah, al
-    inc di
-    inc di
-    mov al, [es:di]
+    mov dh, al
+    mov ax, [es:di]
+    stosw
     call hexascii
-    or al, ah
-    inc di
-    inc di
+    and al, 0x0f
+    or al, dh
     mov [si], al
     inc si
     dec cl
@@ -222,32 +196,35 @@ read0:
     ret
 
 type:
-    pusha
-    or ah, COLOR_ASCII
-    mov dh, al
-    cmp al, 0x20
-    jae type0
-    mov al, 0xf9
-type0:
-    mov [es:di], ax
-    call swap
-    xor ah, COLOR_HEX_A ^ COLOR_ASCII
+    push ax
+    or ah, BCOLORAS
     push di
-    shr di, 0x3
-    jnc type1
-    xor ah, COLOR_HEX_A ^ COLOR_HEX_B
-type1:
+    stosw
+    xor ah, BCOLORAS ^ BCOLORH1
+    shr di, 0x2
+    jnc type0
+    xor ah, BCOLORH1 ^ BCOLORH2
+type0:
     pop di
-    mov al, dh
+    call swap
+    push ax
     shr al, 0x4
     call hexascii
     stosw
-    mov al, dh
+    pop ax
     and al, 0x0f
     call hexascii
     stosw
     call swap
-    popa
+    pop ax
+    ret
+
+swap:
+    cmp di, 0x48
+    jl swap0
+    shr di, 0x2
+swap0:
+    shl di, 0x1
     ret
 
 hexascii:
@@ -277,36 +254,7 @@ CHS_END: db 0x0, 0x0, 0x0
 LBA_START: dd 0x0
 VOLUME_SIZE: dd SECTORS
 
-swap:
-    push ax
-    mov ax, es
-    sub al, 0xa*0x8
-    jb swap1
-    shr di, 0x2
-    shl di, 0x1
-    shr al, 0x2
-    jnc swap0
-    sub al, 0x2
-    add di, 0x40
-swap0:
-    shl al, 0x1
-    jmp swap3
-swap1:
-    shl al, 0x1
-    cmp di, 0x40
-    jb swap2
-    add al, 0xa
-    sub di, 0x40
-swap2:
-    shl di, 0x1
-    add al, 0xa*0x18
-swap3:
-    mov es, ax
-    pop ax
-    ret
-
-
-times 0x1fe - ($ - $$) db 0x0
+times 0x1fe-($-$$) db 0x0
 db 0x55, 0xaa
 
-times 0x200 * SECTORS - ($-$$) db 0x0
+times (SECTORS - 0x1) * 0x200 db 0x0
